6a29f1f6e9388d8b41208f04fe19bb62e6584902
rwx-777 <joshuajared007@gmail.com>
2019-12-06 14:12:55 +0100

    ForLoop and sizeofArrays

--- ForLoops.c
+++ ForLoops.c
@@ -3,9 +3,20 @@
 
 int main() {
 
-    int i = 1;
+    //Basic For Loop
+    for(int i = 1; i <= 5; i++) {
+        printf("%d\n", i);
+    }
+    printf("Finished with first Loop\n");
 
-    
+    //For Loops with Arrays
+    int luckyNumbers[] = {4,8,12,16,32,46};
+
+    //to get the length of an Array we use (sizeof(Array)/sizeof(*Array))
+    int j;
+    for(j=0;j < (sizeof(luckyNumbers)/sizeof(*luckyNumbers)); j++){
+        printf("%d\n", luckyNumbers[j]);
+    }
 
     return 0;
 }
