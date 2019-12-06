1a6b7a5bb4aeb7f0be1f114f178f2ac41a3064e6
rwx-777 <joshuajared007@gmail.com>
2019-12-06 10:21:37 +0100

    basic return stuff and prototyping

--- Basic-returnFunctions.c
+++ Basic-returnFunctions.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Prototyping
+double cube (double num);
+
+
 int main() {
 
+    printf("Answer: %f\n", cube(3.0));
+
     return 0;
+}
+
+double cube (double num) {
+    double result = num * num * num;
+    return result;
+    //return breaks out of function everything after return will not get executed
 }
