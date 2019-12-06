b34d77d9d55bcec58ca48bb1f56cdf518abf853c
rwx-777 <joshuajared007@gmail.com>
2019-12-06 11:27:27 +0100

    new and improved if statments

--- Basic-IfStatements2.c
+++ Basic-IfStatements2.c
@@ -2,19 +2,24 @@
 #include <stdlib.h>
 
 //compares two numbers and stores the largest one
-int max(int num1, int num2){
+int max(int num1, int num2, int num3){
     int result;
-    if (num1 > num2){
+    //&& stands for and
+    // || stands for or 
+    if(num1 >= num2 && num1 >= num3) {
         result = num1;
-    }else {
+    }else if(num2 >= num1 && num2 >= num3) {
         result = num2;
+    }else{
+        result = num3;
     }
+
+
     return result;
 }
 
 int main() {
 
-    printf("%d", max(4,10)); //prints out result
-
+    printf("%d\n", max(4,10,22)); //prints out result
     return 0;
 }
